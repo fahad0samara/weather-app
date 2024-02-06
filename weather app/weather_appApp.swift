bb66f9ade2bce0f704f1dd9250530237982b8e0a
fahad0samarar <fah8ad@outlook.com>
2024-02-06 21:27:02 +0200

    Initial Commit

---
+++ weather app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather app
+//
+//  Created by fahad samara on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
