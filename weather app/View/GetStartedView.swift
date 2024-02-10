84bbf6e55e71a6adcc1d3ece9f979b500e024c38
fahad0samarar <fah8ad@outlook.com>
2024-02-10 17:17:49 +0200

    imrove the styule and add viewfor the  getstartd

---
+++ weather app/View/GetStartedView.swift
@@ -0,0 +1,18 @@
+//
+//  GetStartedView.swift
+//  weather app
+//
+//  Created by fahad samara on 2/10/24.
+//
+
+import SwiftUI
+
+struct GetStartedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GetStartedView()
+}
