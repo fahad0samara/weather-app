6e07aa691707ddbb7e456a7dc5f7f9bf63205932
fahad0samarar <fah8ad@outlook.com>
2024-02-10 14:45:49 +0200

    add the mvvm

---
+++ weather app/Model/WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  weather app
+//
+//  Created by fahad samara on 2/10/24.
+//
+
+import Foundation
---
+++ weather app/ViewModel/WeatherViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherViewModel.swift
+//  weather app
+//
+//  Created by fahad samara on 2/10/24.
+//
+
+import Foundation
